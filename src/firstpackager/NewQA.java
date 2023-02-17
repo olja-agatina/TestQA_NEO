79c8bb9da4fd2ad6843a68124588faa8863ebaf8
olja-agatina <oliaagatina@gmail.com>
2023-02-17 17:41:02 +0200

    Change path chromedriver(line 14). Add var baseUrl(line 18) Change verify title(line 22-27)

--- src/firstpackager/NewQA.java
+++ src/firstpackager/NewQA.java
@@ -2,26 +2,30 @@ package firstpackager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.*;
 import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+//import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class NewQA {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-        System.getProperty("webdriver.chrome.driver","C:\\Users\\Admin\\Downloads\\ide\\chromedriver.exe"); //Used chromedriver
+        System.getProperty("webdriver.chrome.driver","<ChromeDriver_Path>/chromedriver"); //Used chromedriver
+        String baseUrl = "https://web.neobank.one/auth/login";
         
         ChromeDriver driver = new ChromeDriver(); //Create an instance of the class
-        driver.get("https://web.neobank.one/auth/login"); //Use the GET method to open the url
+        driver.get(baseUrl); //Use the GET method to open the url
         
         TimeUnit.SECONDS.sleep(3); //Waiting for the page to load
         
-        String a = "NEOBANK для бізнесу"; //Expected title
-        String b = driver.getTitle(); //Result name
-        if(a.equals(b)) {
-        	System.out.println("Title valid "); // Prints true
-        }else {
-        	System.out.println("Title not valid"); // Prints false
+        if(driver.getTitle().contains("NEOBANK для бізнесу")) {
+        	System.out.println("Title valid ");
         }
-        
+        else {
+        	System.out.println("Title not valid");
+        }
+        		
         driver.findElement(By.id("login")).sendKeys("63 690 16 99"); //Writing a phone number
         driver.findElement(By.className("btn-primary")).click(); //Click button "Continue"
         
